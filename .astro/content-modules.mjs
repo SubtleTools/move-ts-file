ea09cb17eb4e40941a341055ba96e68c31f7d48f
Saulo Vallory <me@saulo.engineer>
2025-08-26 02:09:15 -0300

    chore: remove unnecessary extra docs build when releasing
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,12 @@
 
 export default new Map([
 ["docs/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["docs/content/docs/cli-reference.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fcli-reference.mdx&astroContentModuleFlag=true")],
 ["docs/content/docs/introduction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fintroduction.mdx&astroContentModuleFlag=true")],
-["docs/content/docs/installation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Finstallation.mdx&astroContentModuleFlag=true")],
-["docs/content/docs/troubleshooting.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Ftroubleshooting.mdx&astroContentModuleFlag=true")],
-["docs/content/docs/features/barrel-exports.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Ffeatures%2Fbarrel-exports.mdx&astroContentModuleFlag=true")],
+["docs/content/docs/cli-reference.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fcli-reference.mdx&astroContentModuleFlag=true")],
 ["docs/content/docs/quick-start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fquick-start.mdx&astroContentModuleFlag=true")],
+["docs/content/docs/installation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Finstallation.mdx&astroContentModuleFlag=true")],
+["docs/content/docs/claude/refactoring.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fclaude%2Frefactoring.mdx&astroContentModuleFlag=true")],
+["docs/content/docs/troubleshooting.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Ftroubleshooting.mdx&astroContentModuleFlag=true")],
 ["docs/content/docs/features/import-styles.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Ffeatures%2Fimport-styles.mdx&astroContentModuleFlag=true")],
-["docs/content/docs/claude/refactoring.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Fclaude%2Frefactoring.mdx&astroContentModuleFlag=true")]]);
+["docs/content/docs/features/barrel-exports.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=docs%2Fcontent%2Fdocs%2Ffeatures%2Fbarrel-exports.mdx&astroContentModuleFlag=true")]]);
 		
